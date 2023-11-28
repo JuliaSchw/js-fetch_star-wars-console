ef9960071639c7b3f52dc7503fe74b92d17a1401
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-11-28 13:11:44 +0100

    complete challenge

--- js/index.js
+++ js/index.js
@@ -2,6 +2,23 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data),
+    console.log(
+      data.results.forEach((entry) => {
+        console.log("Name:", entry.name);
+      })
+    );
+  console.log("First Name:", data.results[0].name),
+    console.log(
+      data.results.forEach((entry) => {
+        console.log("Mass:", entry.mass);
+      })
+    );
+  console.log("R2-D2 Eye Color:", data.results[2].eye_color);
+  return data;
+}
 
 fetchData();
